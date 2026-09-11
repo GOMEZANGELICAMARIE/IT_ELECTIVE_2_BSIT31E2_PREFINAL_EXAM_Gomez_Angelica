c92781097643617c56322d0010cefd20506b887d
GOMEZANGELICAMARIE <angelicamariegomez1@gmail.com>
2026-09-11 22:25:01 +0800

    Q2

--- WebApplication1/Models/ExamRepository.cs
+++ WebApplication1/Models/ExamRepository.cs
@@ -12,7 +12,14 @@
                 SelectedAnswer = "C. It allows data to persist after the application stops",
                 Explanation = "Databases provide persistent storage, retaining data after the process ends."
             },
-            
+            new ExamQuestion
+            {
+                Id = 2,
+                Question = "Which approach is being used when an existing database is used to generate EF Core entity classes?",
+                Options = new List<string>{ "A. Code-First", "B. Database-First", "C. Model-First", "D. Controller-First" },
+                SelectedAnswer = "B. Database-First",
+                Explanation = "Database-First approach generates EF Core entity classes and DbContext from an existing database schema."
+            },
         };
     }
 }
