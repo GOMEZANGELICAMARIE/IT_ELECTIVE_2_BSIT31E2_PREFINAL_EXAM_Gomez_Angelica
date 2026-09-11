7b41f223a8565f4339e791210e66948de4225cf2
GOMEZANGELICAMARIE <angelicamariegomez1@gmail.com>
2026-09-11 22:22:36 +0800

    Q1

---
+++ WebApplication1/Models/ExamQuestion.cs
@@ -0,0 +1,12 @@
+﻿namespace IT_ELECTIVE_2_BSIT312E_PREFINAL_EXAM_Lastname_FirstName.Models
+{
+    public class ExamQuestion
+    {
+        public int Id { get; set; }
+        public string Question { get; set; } = string.Empty;
+        public List<string> Options { get; set; } = new();
+        public string SelectedAnswer { get; set; } = string.Empty;
+        public string CorrectAnswer { get; set; } = string.Empty;
+        public string Explanation { get; set; } = string.Empty;
+    }
+}
---
+++ WebApplication1/Models/ExamRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace IT_ELECTIVE_2_BSIT312E_PREFINAL_EXAM_Lastname_FirstName.Models
+{
+    public static class ExamRepository
+    {
+        public static List<ExamQuestion> Questions = new List<ExamQuestion>
+        {
+            new ExamQuestion
+            {
+                Id = 1,
+                Question = "What is the main problem solved by using a database instead of an in-memory collection?",
+                Options = new List<string>{ "A. It makes C# code shorter", "B. It prevents the application from restarting", "C. It allows data to persist after the application stops", "D. It removes the need for MVC" },
+                SelectedAnswer = "C. It allows data to persist after the application stops",
+                Explanation = "Databases provide persistent storage, retaining data after the process ends."
+            },
+            
+        };
+    }
+}
